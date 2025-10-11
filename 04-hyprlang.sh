2f0e4002347c30720f6d41a8b8d056e348c42fff
Shionn <shionn@gmail.com>
2025-10-11 14:59:58 +0000

    debut hyprlang

---
+++ 04-hyprlang.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+version="0.6.4"
+project="hyprlang"
+
+#apt install libpugixml-dev -y
+
+rm -Rf $project*	
+
+git clone --recursive -b v$version https://github.com/hyprwm/${project}.git ${project}
+cd ${project} 
+
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --build ./build --config Release --target hyprlang -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
+#cmake --install build
+
+exit
+
+mkdir ${project}-${version}
+cd ${project}-${version}
+mkdir -p usr/bin
+cp ../build/hyprwayland-scanner usr/bin/
+
+mkdir DEBIAN
+
+echo "Source: ${project}
+Section: graphics
+Priority: optional
+Maintainer: Shionn<shionn@gmail.com>
+Homepage: https://github.com/hyprwm/${project}
+Package: ${project}
+Version: ${version}
+Architecture: amd64
+Depends: 
+Description: The hypr configuration language is an extremely efficient, yet easy to work with, configuration language for linux applications.
+ It's user-friendly, easy to grasp, and easy to implement." >> DEBIAN/control
+
+echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: ${project}
+Source: https://github.com/hyprwm/${project}/tree/v${version}
+Files: *
+Copyright: 2024, Hypr Development
+Licence: BSD 3-Clause License" >> DEBIAN/copyright
+
+cd .. 
+dpkg -b ${project}-${version}
+
+mv ${project}-${version}.deb ../
+
