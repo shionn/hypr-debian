b063be443fe50c1a7124efdf98c0a5320dfe27a4
Shionn <shionn@gmail.com>
2025-10-11 11:59:33 +0000

    debut script 2

---
+++ 02-hyprutils.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+version="0.10.0"
+project="hyprutils"
+
+apt install libpixman-1-dev -y
+
+rm -Rf $project*	
+
+git clone --recursive -b v$version https://github.com/hyprwm/${project}.git ${project}
+cd ${project} 
+
+
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF`
+#cmake --install build
+
+mkdir ${project}-${version}
+cd ${project}-${version}
+mkdir -p usr/bin
+cp ../build/hyprwayland-scanner usr/bin/
+
+mkdir DEBIAN
+
+echo "Source: ${project}
+Section: graphics
+Priority: optional
+Maintainer: Shionn<shionn@gmail.com>
+Homepage: https://github.com/hyprwm/hyprwayland-scanner
+Package: ${project}
+Version: ${version}
+Architecture: amd64
+Depends: libpixman-1-0
+Description: A Hyprland implementation of wayland-scanner, in and for C++." >> DEBIAN/control
+
+echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: ${project}
+Source: https://github.com/hyprwm/hyprwayland-scanner/tree/v0.4.5
+Files: *
+Copyright: 2024, Hypr Development
+Licence: BSD 3-Clause License" >> DEBIAN/copyright
+
+cd .. 
+dpkg -b ${project}-${version}
+
+mv ${project}-${version}.deb ../
+
