78ea456edae769b280163b6b9673990650502c5d
Shionn <shionn@gmail.com>
2025-10-11 20:34:19 +0000

    plus de dependance

--- 07-hyprland.sh
+++ 07-hyprland.sh
@@ -2,7 +2,7 @@
 version="0.51.1"
 project="Hyprland"
 
-apt install libgles2-mesa-dev libcairo2-dev libinput-dev wayland-protocols libdrm-dev libpixman-1-dev -y
+apt install libgles2-mesa-dev libcairo2-dev libinput-dev wayland-protocols libdrm-dev libpixman-1-dev libxkbcommon-dev libre2-dev libwayland-dev libgbm-dev libpango1.0-dev libxcursor-dev libxcb-xfixes0-dev libxcb-icccm4-dev libxcb-composite0-dev libxcb-res0-dev libxcb-errors-dev libtomlplusplus-dev libpugixml-dev -y
 
 #rm -Rf $project*	
 
@@ -29,7 +29,7 @@ Homepage: https://github.com/hyprwm/${project}
 Package: ${project}
 Version: ${version}
 Architecture: amd64
-Depends: aquamarine (>=0.9.3), hyprlang (>=0.3.2), hyprcursor (>=0.1.7), hyprutils (>=0.8.2), hyprgraphics (>=0.1.3), hyprwayland-scanner (>=0.3.10)
+Depends: aquamarine (>=0.9.3), hyprlang (>=0.3.2), hyprcursor (>=0.1.7), hyprutils (>=0.8.2), hyprgraphics (>=0.1.3), hyprwayland-scanner (>=0.3.10), libxkbcommon0, libre2-11, libgbm1, pango1.0-tools, libxcursor1, libxcb-xfixes0, libxcb-icccm4, libxcb-composite0, libxcb-res0, libxcb-errors0
 Description: Hyprland is a 100% independent, dynamic tiling Wayland compositor that doesn't sacrifice on its looks.
  It provides the latest Wayland features, is highly customizable, has all the eyecandy, the most powerful plugins, easy IPC, much more QoL stuff than other compositors and more..." >> DEBIAN/control
 
--- 99-clean.sh
+++ 99-clean.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-apt remove libpugixml-dev libpixman-1-dev libgles2-mesa-dev libseat-dev libinput-dev wayland-protocols libwayland-dev libdrm-dev libgbm-dev libdisplay-info-dev hwdata libcairo2-dev libzip-dev libtomlplusplus-dev libmagic-dev libwebp-dev librsvg2-dev libjpeg-dev -y
+#!/bin/bash 
+apt remove libpugixml-dev libpixman-1-dev libgles2-mesa-dev libseat-dev libinput-dev wayland-protocols libwayland-dev libdrm-dev libgbm-dev libdisplay-info-dev hwdata libcairo2-dev libzip-dev libtomlplusplus-dev libmagic-dev libwebp-dev librsvg2-dev libjpeg-dev libxkbcommon-dev libre2-dev libwayland-dev libgbm-dev libpango1.0-dev libxcursor-dev libxcb-xfixes0-dev libxcb-icccm4-dev libxcb-composite0-dev libxcb-res0-dev libxcb-errors-dev -y
 apt remove cmake build-essential pkg-config lintian -y
 apt autoremove -y
 
