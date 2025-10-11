16a6eed30b840a74a6287443ecfb498846aca8ef
Shionn <shionn@gmail.com>
2025-10-11 13:46:53 +0000

    debut aquamarine

---
+++ 03-aquamarine.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+version="0.9.5"
+project="aquamarine"
+
+#apt install libpixman-1-dev -y
+
+rm -Rf $project*	
+
+git clone --recursive -b v$version https://github.com/hyprwm/${project}.git ${project}
+cd ${project} 
+
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
+#cmake --install build
+
+exit
+
+mkdir ${project}-${version}
+cd ${project}-${version}
+mkdir -p usr/bin/x86_64-linux-gnu/
+mv ../build/libhyprutils.so* usr/bin/x86_64-linux-gnu/
+
+mkdir DEBIAN
+
+echo "Source: ${project}
+Section: graphics
+Priority: optional
+Maintainer: Shionn<shionn@gmail.com>
+Homepage: https://github.com/hyprwm/${project}
+Package: ${project}
+Version: ${version}
+Architecture: amd64
+Depends: libpixman-1-0
+Description: Aquamarine is a very light linux rendering backend library. It provides basic abstractions for an application to render on a Wayland session (in a window) or a native DRM session.
+
+It is agnostic of the rendering API (Vulkan/OpenGL) and designed to be lightweight, performant, and minimal.
+
+Aquamarine provides no bindings for other languages. It is C++-only." >> DEBIAN/control
+
+echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: ${project}
+Source: https://github.com/hyprwm/${project}/tree/v${version}
+Files: *
+Copyright: 2024, Hypr Development
+Licence: BSD 3-Clause License" >> DEBIAN/copyright
+
+cd .. 
+dpkg -b ${project}-${version}
+
+mv ${project}-${version}.deb ../
+
