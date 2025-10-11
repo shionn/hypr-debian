15cb9581397b814dc0a9ddd405bcf58b40688a4c
Shionn <shionn@gmail.com>
2025-10-11 11:48:07 +0000

    premier package ?

--- 00-build-dependency.sh
+++ 00-build-dependency.sh
@@ -3,5 +3,5 @@ apt update
 apt install git cmake build-essential  -y
 # poru resoudre les dependance CMAKE
 apt install pkg-config -y
-# pour construire les package
-#apt install devscripts dh-cmake -y
+# pour verifier la construction du packe
+apt install lintian -y
--- 01-hyprwayland-scanner.sh
+++ 01-hyprwayland-scanner.sh
@@ -23,8 +23,6 @@ echo "Source: ${project}
 Section: graphics
 Priority: optional
 Maintainer: Shionn<shionn@gmail.com>
-Build-Depends: cmake, libpugixml-dev
-Standards-Version: ${version}
 Homepage: https://github.com/hyprwm/hyprwayland-scanner
 Package: ${project}
 Version: ${version}
@@ -32,43 +30,15 @@ Architecture: amd64
 Depends: libpugixml1v5
 Description: A Hyprland implementation of wayland-scanner, in and for C++." >> DEBIAN/control
 
-
-#echo "${project} (${version}) unstable; urgency=low 
-
-# [ Shionn ]
-# * release ${version}
-
-#-- Shionn <shionn@gmail.com> $(date)" > debian/changelog
-
-#echo "#!/usr/bin/make -f
-
-#%:
-#	dh \$@
-#
-#override_dh_auto_install:
-#	dh_auto_install --destdir=\$(DEB_DESTDIR)
-
-#build:
-#	cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
-#	cmake --build build -j `nproc`
-#
-#install:
-#	cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
-#	cmake --build build -j `nproc`
-#	cmake --install build
-	
-#clean:
-#	rm -rf build
-#" > debian/rules
-
-#echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
-#Upstream-Name: ${project}
-#Source: https://github.com/hyprwm/hyprwayland-scanner/tree/v0.4.5
-#
-#Licence: BSD 3-Clause License
-#Files: *
-#Copyright: 2024, Hypr Development" > debian/copyright
+echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: ${project}
+Source: https://github.com/hyprwm/hyprwayland-scanner/tree/v0.4.5
+Files: *
+Copyright: 2024, Hypr Development
+Licence: BSD 3-Clause License" >> DEBIAN/copyright
 
 cd .. 
 dpkg -b ${project}-${version}
+
+mv ${project}-${version}.deb ../
 
