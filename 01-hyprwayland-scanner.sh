98df85a5f03a3e3f72d61e1fdedba9f310253fa2
Shionn <shionn@gmail.com>
2025-10-11 11:35:19 +0000

    premier build

--- 01-hyprwayland-scanner.sh
+++ 01-hyprwayland-scanner.sh
@@ -17,60 +17,58 @@ cd ${project}-${version}
 mkdir -p usr/bin
 cp ../build/hyprwayland-scanner usr/bin/
 
-
-
-
-mkdir debian
+mkdir DEBIAN
 
 echo "Source: ${project}
 Section: graphics
 Priority: optional
 Maintainer: Shionn<shionn@gmail.com>
-Build-Depends: cmake, debhelper, dh-cmake, libpugixml-dev
+Build-Depends: cmake, libpugixml-dev
 Standards-Version: ${version}
 Homepage: https://github.com/hyprwm/hyprwayland-scanner
-
 Package: ${project}
+Version: ${version}
 Architecture: amd64
 Depends: libpugixml1v5
-Description: A Hyprland implementation of wayland-scanner, in and for C++." >> debian/control
+Description: A Hyprland implementation of wayland-scanner, in and for C++." >> DEBIAN/control
 
 
-echo "${project} (${version}) unstable; urgency=low 
+#echo "${project} (${version}) unstable; urgency=low 
 
- [ Shionn ]
- * release ${version}
+# [ Shionn ]
+# * release ${version}
 
--- Shionn <shionn@gmail.com> $(date)" > debian/changelog
+#-- Shionn <shionn@gmail.com> $(date)" > debian/changelog
 
-echo "#!/usr/bin/make -f
+#echo "#!/usr/bin/make -f
 
-%:
-	dh \$@
+#%:
+#	dh \$@
+#
+#override_dh_auto_install:
+#	dh_auto_install --destdir=\$(DEB_DESTDIR)
 
-override_dh_auto_install:
-	dh_auto_install --destdir=\$(DEB_DESTDIR)
-
-build:
-	cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
-	cmake --build build -j `nproc`
-
-install:
-	cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
-	cmake --build build -j `nproc`
-	cmake --install build
+#build:
+#	cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
+#	cmake --build build -j `nproc`
+#
+#install:
+#	cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
+#	cmake --build build -j `nproc`
+#	cmake --install build
 	
-clean:
-	rm -rf build
-" > debian/rules
+#clean:
+#	rm -rf build
+#" > debian/rules
 
-echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
-Upstream-Name: ${project}
-Source: https://github.com/hyprwm/hyprwayland-scanner/tree/v0.4.5
+#echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+#Upstream-Name: ${project}
+#Source: https://github.com/hyprwm/hyprwayland-scanner/tree/v0.4.5
+#
+#Licence: BSD 3-Clause License
+#Files: *
+#Copyright: 2024, Hypr Development" > debian/copyright
 
-Licence: BSD 3-Clause License
-Files: *
-Copyright: 2024, Hypr Development" > debian/copyright
-
-
+cd .. 
+dpkg -b ${project}-${version}
 
