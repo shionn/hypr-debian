756615012a616c37059c8dc3e9dfc0241c9c4e28
Shionn <shionn@gmail.com>
2025-10-14 16:00:33 +0000

    tentative qml

--- src/20-hyprpolkitagent.sh
+++ src/20-hyprpolkitagent.sh
@@ -11,7 +11,7 @@ rm -Rf $project*
 git clone --recursive -b v$version https://github.com/hyprwm/${project}.git ${project}
 cd ${project} 
 
-cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -DINSTALL_QML_PREFIX=/lib/x86_64-linux-gnu/qt6/qml -S . -B ./build
 cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF`
 cmake --install build
 
