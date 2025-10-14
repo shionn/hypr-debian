f1dfd63b4a14d9acd772851f2b7d5ed9433c5023
Shionn <shionn@gmail.com>
2025-10-14 18:45:01 +0000

    premiere version de hyprland polkit

--- src/99-clean.sh
+++ src/99-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-apt remove *-dev wayland-protocols hwdata qml6-* -y
+apt remove *-dev wayland-protocols hwdata qml6-* qt6-* qml-qt6 -y
 apt remove cmake build-essential pkg-config lintian -y
 apt autoremove -y
 
