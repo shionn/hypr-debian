e04cec790b1c2b0a0819c3d5a3744abe17465308
Shionn <shionn@gmail.com>
2025-10-20 21:24:14 +0000

    hypridle

--- src/10-hypridle.sh
+++ src/10-hypridle.sh
@@ -5,6 +5,7 @@ revision="0~pre1"
 
 apt install libwayland-dev \
 	wayland-protocols \
+	libpugixml-dev \
 	libsdbus-c++-dev -y
 
 cd build
@@ -15,19 +16,16 @@ cd ${project}
 
 cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
 cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF`
-
-exit 
-
 cmake --install build
 
 mkdir ${project}-${version}-${revision}
 cd ${project}-${version}-${revision}
-mkdir -p usr/local/bin/
-mkdir -p usr/local/etc/pam.d/
-mkdir -p usr/local/share/hypr/
-cp ../build/hyprlock  usr/local/bin/
-cp ../pam/hyprlock usr/local/etc/pam.d/
-cp ../assets/example.conf usr/local/share/hypr/hyprlock.conf
+mkdir -p usr/bin/
+mkdir -p usr/lib/systemd/user/
+mkdir -p usr/share/hypr/
+cp ../build/hypridle usr/bin/
+cp ../build/systemd/hypridle.service usr/lib/systemd/user/
+cp ../assets/example.conf usr/share/hypr/hypridle.conf
 
 mkdir DEBIAN
 
@@ -39,7 +37,7 @@ Homepage: https://github.com/hyprwm/${project}
 Package: ${project}
 Version: ${version}-${revision}
 Architecture: amd64
-Depends: hyprlang (>=0.6.0), hyprutils (>=0.2.0), hyprwayland-scanner (>=0.4.4)
+Depends: hyprlang (>=0.6.0), hyprutils (>=0.2.0), hyprwayland-scanner (>=0.4.4), hyprland-protocols (>=0.6.0), libsdbus-c++2 (>= 0.2.0), libwayland-client0, wayland-protocols, libpugixml1v5
 Description: Hyprland's idle daemon.">> DEBIAN/control
 
 echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
