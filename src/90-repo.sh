465a017d7b4374fcdfece00f4aee7e9f1e26ee0d
Shionn <shionn@gmail.com>
2025-10-22 11:46:35 +0000

    Tentativ de repo Experimental

--- src/90-repo.sh
+++ src/90-repo.sh
@@ -8,12 +8,28 @@ Codename: forky
 Architectures: amd64 i386
 Components: main
 Description: Hyprland repository by shionn
-SignWith: yes" > docs/conf/distributions
+SignWith: yes
 
-reprepro -b docs/ includedeb forky build/*.deb
+Origin: shionn.github.io/hypr-debian
+Label: shionn.github.io/hypr-debian
+Codename: experimental
+Architectures: amd64 i386
+Components: main
+Description: Hyprland repository by shionn
+SignWith: yes
+" > docs/conf/distributions
 
+mkdir -p build/experimental
+mkdir -p build/forky
 
+mv build/*~*.deb build/experimental/
+mv build/*.deb build/forky/
 
+reprepro -b docs/ includedeb forky build/forky/*.deb
+reprepro -b docs/ includedeb experimental build/experimental/*.deb
+
+rm build/experimental/*.deb
+rm build/forky/*.deb
 
 
 
