56c61dbdff511b1a2c0f74e375529996fd977ab5
Shionn <shionn@gmail.com>
2025-11-02 15:52:42 +0000

    hyprshot

---
+++ src/70-hyprshot.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+version="1.3.0"
+project="hyprshot"
+revision="0~pre0"
+
+cd build
+rm -Rf $project*	
+
+git clone -b $version git@github.com:Gustash/Hyprshot.git ${project}
+cd ${project} 
+
+mkdir ${project}-${version}-${revision}
+cd ${project}-${version}-${revision}
+mkdir -p usr/local/bin/
+cp ../hyprshot usr/local/bin/
+
+mkdir DEBIAN
+
+echo "Source: ${project}
+Section: graphics
+Priority: optional
+Maintainer: Shionn<shionn@gmail.com>
+Homepage: https://github.com/Gustash/Hyprshot
+Package: ${project}
+Version: ${version}-${revision}
+Architecture: amd64
+Depends: hyprland 
+Description: Hyprshot is an utility to easily take screenshot in Hyprland using your mouse.">> DEBIAN/control
+
+echo "Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: ${project}
+Source: https://github.com/Gustash/Hyprshot/tree/${version}
+Files: *
+Copyright: 
+Licence: GNU GPL V3" >> DEBIAN/copyright
+
+cd .. 
+dpkg -b ${project}-${version}-${revision}
+
+mv ${project}-${version}-${revision}.deb ../
+
